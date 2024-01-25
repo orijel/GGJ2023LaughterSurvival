0c5cb53e80b489be6bf0962143213a5dcec544ba
orijel <orijel1@gmail.com>
2024-01-26 01:39:51 +0200

    add some methods to weapon

--- LaughterSurvival/Assets/Resources/Scripts/Weapons/WeaponBase.cs
+++ LaughterSurvival/Assets/Resources/Scripts/Weapons/WeaponBase.cs
@@ -2,14 +2,37 @@ using Assets.Resources.Scripts;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public abstract class WeaponBase : MonoBehaviour
 {
     [SerializeField] private float _damage;
     [SerializeField] private DamageType _damageType;
+    [SerializeField] private UnityEvent _onAttack;
+    [SerializeField] private Collider _Collider;
+
+    private void Start()
+    {
+        _Collider.enabled = false;
+    }
 
     public void HitEnemey(EnemyBase enemy)
     {
         enemy.OnHit(this);
+    }
+
+    public void Attack()
+    {
+        _onAttack.Invoke();
+    }
+
+    public void ActivateTrigger()
+    {
+        _Collider.enabled = true;
+    }
+
+    public void DeactivateTrigger()
+    {
+        _Collider.enabled = false;
     }
 }
