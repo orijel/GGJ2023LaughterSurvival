af321bf5594c45b30134b66c4f778584153e71e7
eliminator <elientelis1999@gmail.com>
2024-01-26 20:17:08 +0200

    spawner added

--- LaughterSurvival/Assets/Resources/Scripts/Spawner.cs
+++ LaughterSurvival/Assets/Resources/Scripts/Spawner.cs
@@ -5,34 +5,42 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] List<Transform> transforms;
-    [SerializeField] GameObject objectTospawn;
-
+    [SerializeField] ObjectPool objectPool;
     private System.Random _random = new System.Random();
-
+    private float timer = 0;
+    private float interval = 10;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void FixedUpdate()
     {
-        // Create an instance of the Random class
+        // Accumulate time
+        timer += Time.fixedDeltaTime;
 
-        // Generate a random number between 0 (inclusive) and 11 (exclusive)
-        int spawnIndex = _random.Next(0, transforms.Count);
-        Spawn(spawnIndex);
+        // Check if 10 seconds have passed
+        if (timer >= interval)
+        {
+            // Reset the timer
+            timer = 0;
+            // Create an instance of the Random class
+
+            // Generate a random number between 0 (inclusive) and 11 (exclusive)
+            int spawnIndex = _random.Next(0, transforms.Count);
+
+            Spawn(spawnIndex);
+        }
     }
 
     void Spawn(int spawnIndex)
     {
-        Instantiate(objectTospawn, transforms[spawnIndex]);
+        foreach (var obj in objectPool.pool) { 
+            if(obj.activeInHierarchy == false)
+            {
+                obj.SetActive(true);
+                obj.transform.position = transforms[spawnIndex].position;
+                return;
+            }
+        }
+        //Instantiate(objectPool.pool[0], transforms[spawnIndex]);
     }
 
 
