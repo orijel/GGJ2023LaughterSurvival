0f9871f277b2ad0708f40dcbe0edcb422e6fa84d
orijel <orijel1@gmail.com>
2024-01-27 12:57:06 +0200

    make gun work

---
+++ LaughterSurvival/Assets/Resources/Scripts/SetTransformAroundCollider.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTransformAroundCollider : MonoBehaviour
+{
+    [SerializeField] private Camera _playerCamera;
+    [SerializeField] private Vector3 _planeNormal;
+    [SerializeField] private Vector3 _planePoint;
+    [SerializeField] private Collider _collider;
+    [SerializeField] private Transform _transformToMove;
+
+    private Plane _plane;
+
+    private void Awake()
+    {
+        _plane = new Plane(_planeNormal, _planePoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray mouseRay = _playerCamera.ScreenPointToRay(Input.mousePosition);
+        if (_plane.Raycast(mouseRay, out var enter))
+        {
+            Vector3 pointOnPlane = mouseRay.GetPoint(enter);
+            Vector3 pointOnCollider = _collider.ClosestPoint(pointOnPlane);
+            _transformToMove.position = pointOnCollider;
+
+            //Vector3 pointToLookAt = _colliderToLookAt.ClosestPoint(pointOnCollider);
+            _transformToMove.LookAt(pointOnPlane);
+        }
+    }
+}
